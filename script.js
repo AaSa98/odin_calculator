aee25058eded9081674b94c37ceb275824c686b6
AaSa98 <aa.sntmra@gmail.com>
2022-04-15 20:10:39 +0200

    nummern werden bei eingabe geaddet + mit ac wird eingabe wieder auf 0 gesetzt

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-window.onload = function() {
-    var btn_num = document.querySelectorAll(".btn-num");
-    btn_num.forEach(btn => {
-        btn.addEventListener("click", () => writeNum(btn.textContent));
-    });
+let current = document.querySelector(".current");
+console.log(current.textContent);
+
+window.onload = function () {
+  var btn_num = document.querySelectorAll(".btn-num");
+  btn_num.forEach((btn) => {
+    btn.addEventListener("click", () => writeNum(btn.textContent));
+  });
+  var btn_del = document.getElementById("del");
+  btn_del.addEventListener("click", () => del());
+  var btn_ac = document.getElementById("ac");
+  btn_ac.addEventListener("click", () => ac());
+};
+
+function writeNum(numInput) {
+  current.textContent = current.textContent.concat(numInput);
+  console.log(current.textContent);
 }
 
+function del() {}
 
-function writeNum(numInput){
-    const current = document.querySelector(".current");
-    console.log(numInput);
+function ac() {
+  current.textContent = 0;
 }
